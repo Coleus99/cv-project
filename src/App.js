061ef6b84b3c8623d351aee94ae99a18e3360d8f
coleus99 <christopherjohncoles@gmail.com>
2020-11-18 10:46:52 +0000

    Setup Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      {/* <Header /> */}
+      <main>
+        <div class="py-5">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-4">
+                <p>Contact Details Here</p>
+              </div>
+              <div className="col-sm-8">
+                <p>personal statement here</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main> 
+      {/* <Footer /> */}
+    </body>
   );
 }
 
