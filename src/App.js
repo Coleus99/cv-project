3bd5ffbfb65ec17ddc357f1afe878d4b35fa3d7c
coleus99 <christopherjohncoles@gmail.com>
2020-11-26 12:50:57 +0000

    refactored as functional components rather than classes.

--- src/App.js
+++ src/App.js
@@ -5,107 +5,102 @@ import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      editTarget: "",
-      formFields: [],
-      title: {
-        name: ["John Doe","text"],
-        position: ["Digital Designer","text"],
-      },
-      contact: {
-        email: ["john@example.com", "email"],
-        phone: ["02736152361", "number"],
-      },
-      profile: {
-        about: [
-          "I am a real go-getter. I love using React to build projects.",
-          "textarea"
-        ]
-      },
-      education: {
-        qualifications:
-          ["1st Class Degree in Computer Science. A level Maths and Physics", "textarea"],
-      },
-      experience: {
-        details:
-          ["4 years experience as a digital designer, numerous projects built as part of the Odin Project", "textarea"],
-      },
-    };
-  }
+function App(){
+  const[editTarget,setEditTarget] = useState('');
+  const[formFields,setFormFields] = useState([]);
+  const[data,setData] = useState({
+    title: {
+      name: ["John Doe","text"],
+      position: ["Digital Designer","text"],
+    },
+    contact: {
+      email: ["john@example.com", "email"],
+      phone: ["02736152361", "number"],
+    },
+    profile: {
+      about: [
+        "I am a real go-getter. I love using React to build projects.",
+        "textarea"
+      ]
+    },
+    education: {
+      qualifications:
+        ["1st Class Degree in Computer Science. A level Maths and Physics", "textarea"],
+    },
+    experience: {
+      details:
+        ["4 years experience as a digital designer, numerous projects built as part of the Odin Project", "textarea"],
+    }
+  });
 
-  edit = (target) => {
-    let newState = this.state;
-    newState.editTarget = target;
-    newState.formFields = this.state[target];
-    this.setState(newState);
+  function edit(target){
+    setEditTarget(target)
+    setFormFields(data[target])
   };
-  save = (e, data) => {
+  function save(e, editTarget, formData){
     e.preventDefault();
-    let newState = this.state;
-    newState[this.state.editTarget] = data;
-    this.setState(newState);
+    console.log(formData)
+    setData({...data, [editTarget] : formData})
+    // let newState = data;
+    // newState[data.editTarget] = data;
+    // this.setState(newState);
   };
 
-  render() {
-    return (
-      <div>
-        <Header id="title" title={this.state.title} edit={this.edit} />
-        <Form fields={this.state.formFields} save={this.save} editTarget={this.state.editTarget}/>
-        <main>
-          <div className="py-5">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
-                    <Contact
-                      id="contact"
-                      edit={this.edit}
-                      contact={this.state.contact}
-                    />
-                  </div>
-                </div>
-                <div className="col-md-8">
-                  <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
-                    <Profile
-                      id="profile"
-                      edit={this.edit}
-                      profile={this.state.profile}
-                    />
-                  </div>
+  return (
+    <div>
+      <Header id="title" title={data.title} edit={edit} />
+      <Form fields={formFields} save={save} editTarget={editTarget}/>
+      <main>
+        <div className="py-5">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
+                  <Contact
+                    id="contact"
+                    edit={edit}
+                    contact={data.contact}
+                  />
                 </div>
               </div>
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
-                    <Education
-                      id="education"
-                      edit={this.edit}
-                      education={this.state.education}
-                    />
-                  </div>
+              <div className="col-md-8">
+                <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
+                  <Profile
+                    id="profile"
+                    edit={edit}
+                    profile={data.profile}
+                  />
                 </div>
-                <div className="col-md-8">
-                  <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
-                    <Experience
-                      id="experience"
-                      edit={this.edit}
-                      experience={this.state.experience}
-                    />
-                  </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
+                  <Education
+                    id="education"
+                    edit={edit}
+                    education={data.education}
+                  />
+                </div>
+              </div>
+              <div className="col-md-8">
+                <div className="card border-light mb-3" style={{boxShadow: `2px 2px 2px #e9e9e9`}}>
+                  <Experience
+                    id="experience"
+                    edit={edit}
+                    experience={data.experience}
+                  />
                 </div>
               </div>
             </div>
           </div>
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,81 +1,75 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class Form extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      section : {}
+const Form = ( {fields, save, editTarget} ) => {
+  const[formData,setFormData] = useState(fields);
+
+  useEffect (() => {
+    if (fields) {
+      setFormData(fields);
     }
+  }, [fields])
+
+  let storeValue = (e) => {
+    setFormData({...formData, [e.target.id] : [e.target.value]})
   }
-  storeValue = (e) => {
-    let newState=this.state;
-    newState.section[e.target.id][0] = e.target.value;
-    this.setState(newState);
-  }
-  componentDidUpdate(prevProps) {
-    //Typical usage, don't forget to compare the props
-    if (this.props.fields !== prevProps.fields) {
-      let newState = {section : this.props.fields, editTarget : this.properCase(this.props.editTarget)}
-      this.setState(newState)
-    }
-   }
-   properCase = (string) => {
+
+  let properCase = (string) => {
     return string.replace(string[0], string[0].toUpperCase())
    }
-   
-  render(){
-    let input;
-    const editFields = Object.keys(this.props.fields).map(field => {
-      if (this.props.fields[field][1] === "textarea") {
-        input = (
-          <textarea
-            onChange={this.storeValue}
-            type={this.props.fields[field][1]}
-            className="form-control"
-            id={field}
-            value={this.props.fields[field][0] || ""}
-          ></textarea>
-        );
-      } else {
-        input = (
-          <input
-            onChange={this.storeValue}
-            type={this.props.fields[field][1]}
-            className="form-control"
-            id={field}
-            value={this.props.fields[field][0] || ""}
-          />
-        );
-      }
-      return (
-        <div className="form-group" key={field}>
-          <label htmlFor={field}>{this.properCase(field)}</label>
-          {input}
-        </div>
-      ) 
-    })
-    return(
-      <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="editModalLabel">Edit {this.state.editTarget} </h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-            <form onSubmit={(e) => this.props.save(e, this.props.fields)}>
-              {editFields}
-              <button type="submit" className="btn btn-primary mr-2">Submit</button>
-              <button data-dismiss="modal" className="btn btn-secondary">Close</button>
-            </form>
-            </div>
+
+  let input;
+  let editFields;
+  editFields = Object.keys(formData).map(field => {
+    if (formData[field][1] === "textarea") {
+      input = (
+        <textarea
+          onChange={storeValue}
+          type={formData[field][1]}
+          className="form-control"
+          id={field}
+          value={formData[field][0] || ""}
+        ></textarea>
+      );
+    } else {
+      input = (
+        <input
+          onChange={storeValue}
+          type={formData[field][1]}
+          className="form-control"
+          id={field}
+          value={formData[field][0] || ""}
+        />
+      );
+    }
+    return (
+      <div className="form-group" key={field}>
+        <label htmlFor={field}>{properCase(field)}</label>
+        {input}
+      </div>
+    ) 
+  })
+
+  return(
+    <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="editModalLabel">Edit {editTarget} </h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+          <form onSubmit={(e) => save(e, editTarget, formData)}>
+            {editFields}
+            <button type="submit" className="btn btn-primary mr-2">Submit</button>
+            <button data-dismiss="modal" className="btn btn-secondary">Close</button>
+          </form>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Form
